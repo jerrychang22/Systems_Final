d0c8c5b0e7dddbb29255d8dcc3d1c30b5fae263c
Jerry <jerrychang22@gmail.com>
2017-01-09 20:30:57 -0500

    Initialize necessary files

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "network.h"
+
+int main(){
+
+
+  //----------------------Wait for user to input ip and port-----------------------
+
+  //IP
+  printf("Host IP Address (IPv4) : ");
+  char ipinput[32];
+  fgets(ipinput, 32, stdin);
+  //if () {} //Check for valid IPv4 address
+
+  //Port
+  printf("");
+  char portinput[8];
+  fgets(portinput, 8, stdin);
+  //if () {} //Check for valid port
+
+  //--------------------------------------------------------------------------------
+
+  
+  
+  //----------------------------Socket and Connect----------------------------------
+
+
+
+
+
+
+  //--------------------------------------------------------------------------------
+
+
+  //-------------------------------Client-side functions---------------------------
+
+
+
+  //-------------------------------------------------------------------------------
+  
+  
+  
+
+  return 0;
+}
---
+++ host.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "network.h"
+
+int main(){
+  
+
+  return 0;
+}
